84adaefc5f36d800e915c058fe1d924c6156ef74
Ismael G Marin C <ismael.marin@gmail.com>
2026-05-28 20:14:03 -0600

    V1.0.1 gems updated

--- test/evaluator/agent_evaluator_test.rb
+++ test/evaluator/agent_evaluator_test.rb
@@ -11,7 +11,7 @@ module SkillBench
 
     def test_version_is_accessible
       assert(defined?(SkillBench::VERSION))
-      assert_equal '0.1.0', SkillBench::VERSION
+      assert_equal '1.0.1', SkillBench::VERSION
     end
 
     def test_module_is_already_loaded?
